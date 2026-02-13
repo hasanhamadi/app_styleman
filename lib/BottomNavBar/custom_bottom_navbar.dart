11b4a3f044198c37aa7bc1428a9dadc2ea0ee66e
hasanhamadi <hasanias1385@gmail.com>
2026-02-13 20:39:47 +0330

    app_styleman

--- lib/BottomNavBar/custom_bottom_navbar.dart
+++ lib/BottomNavBar/custom_bottom_navbar.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'navigation_cubit.dart';
-
 class CustomBottomNavBar extends StatelessWidget {
   const CustomBottomNavBar({super.key});
 
@@ -14,10 +13,25 @@ class CustomBottomNavBar extends StatelessWidget {
         return BottomNavigationBar(
           currentIndex: state.currentIndex,
           onTap: (index) => context.read<NavigationCubit>().updateIndex(index),
+          // تنظیم نوع برای نمایش بهتر وقتی تعداد آیتم‌ها زیاد می‌شود
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey,
           items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'خانه'),
+            BottomNavigationBarItem(icon: Icon(Icons.home_outlined), activeIcon: Icon(Icons.home), label: 'خانه'),
             BottomNavigationBarItem(icon: Icon(Icons.search), label: 'جستجو'),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'پروفایل'),
+
+            // آیتم جدید: سبد خرید
+            BottomNavigationBarItem(
+                icon: Badge( // اضافه کردن بج برای تعداد محصولات (اختیاری)
+                  label: Text('۰'),
+                  child: Icon(Icons.shopping_cart_outlined),
+                ),
+                activeIcon: Icon(Icons.shopping_cart),
+                label: 'سبد خرید'
+            ),
+
+            BottomNavigationBarItem(icon: Icon(Icons.person_outline), activeIcon: Icon(Icons.person), label: 'پروفایل'),
           ],
         );
       },
